9ee83ebfb9dd0a44f99177407d966dddcfec1c35
Sam Hughes <sam@samuelhughes.com>
2023-07-06 16:28:07 -0700

    Basic command line parsing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+
+struct command_line_args {
+    bool version = false;
+    bool help = false;
+
+    std::vector<std::string> files;
+};
+
+bool parse_command_line(FILE *err_fp, int argc, const char **argv, command_line_args *out) {
+    // TODO: We could check for duplicate or conflicting args (like --help and --version
+    // used together with other args).
+    int i = 1;
+    *out = command_line_args{};
+    while (i < argc) {
+        const char *arg = argv[i];
+        if (0 == strcmp(arg, "--version")) {
+            out->version = true;
+            ++i;
+        } else if (0 == strcmp(arg, "--help")) {
+            out->help = true;
+            ++i;
+        } else if (0 == strcmp(arg, "--")) {
+            ++i;
+            while (i < argc) {
+                out->files.emplace_back(argv[i]);
+                ++i;
+            }
+        } else if (arg[0] == '-') {
+            fprintf(err_fp, "Invalid argument '%s'.  See --help for usage.\n", arg);
+            return false;
+        } else {
+            out->files.emplace_back(arg);
+            ++i;
+        }
+    }
+
+    return true;
+}
 
 int main(int argc, const char **argv) {
-    (void)argc; (void)argv;
+    command_line_args args;
+    if (!parse_command_line(stderr, argc, argv, &args)) {
+        return 2;
+    }
+
     printf("Hello, world!\n");
+
+    printf("version = %d, help = %d, files count = %zu\n", args.version, args.help, args.files.size());
+    for (auto&& f : args.files) {
+        printf("File: %s\n", f.c_str());
+    }
+
     return 0;
 }
