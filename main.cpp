1c02c0c3126e3caef8959092300ff1a34a2feed5
Sam Hughes <sam@samuelhughes.com>
2023-07-06 16:54:21 -0700

    file_descriptor and termios basics

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+// TODO: Remove unused includes^
+
 #include <cstdio>
 #include <cstring>
 #include <string>
@@ -52,6 +59,8 @@ void print_help(FILE *fp) {
     fprintf(fp, "Usage: --help | --version | [files...] [-- files..]\n");
 }
 
+int run_program(const command_line_args& args);
+
 int main(int argc, const char **argv) {
     command_line_args args;
     if (!parse_command_line(stderr, argc, argv, &args)) {
@@ -69,11 +78,58 @@ int main(int argc, const char **argv) {
         return 0;
     }
 
-    printf("Hello, world!\n");
+    return run_program(args);
+}
 
-    printf("version = %d, help = %d, files count = %zu\n", args.version, args.help, args.files.size());
-    for (auto&& f : args.files) {
-        printf("File: %s\n", f.c_str());
+struct file_descriptor {
+    int fd = -1;
+
+    ~file_descriptor() {
+        if (fd != -1) {
+            int discard = ::close(fd);
+            (void)discard;
+            fd = -1;
+        }
+    }
+    int close() {
+        int ret = ::close(fd);
+        fd = -1;
+        return ret;
+    }
+
+    file_descriptor() = default;
+    explicit file_descriptor(int _fd) : fd(_fd) { }
+};
+
+int run_program(const command_line_args& args) {
+    if (args.files.size() > 0) {
+        fprintf(stderr, "File opening (on command line) not supported yet!\n");  // TODO
+        return 1;
+    }
+
+    file_descriptor term{open("/dev/tty", O_RDWR)};
+    if (term.fd == -1) {
+        // TODO: Errno
+        fprintf(stderr, "Could not open tty\n");
+        return 1;
+    }
+
+    struct termios tcattr;
+    int res = tcgetattr(term.fd, &tcattr);
+    if (res == -1) {
+        // TODO: Errno
+        fprintf(stderr, "Could not get tcattr for tty\n");
+        return 1;
+    }
+
+    printf("testing\n");
+    fflush(stdout);
+    usleep(500'000);
+
+    res = term.close();
+    if (res == -1) {
+        fprintf(stderr, "Could not close tty\n");
+        return 1;
     }
 
     return 0;
