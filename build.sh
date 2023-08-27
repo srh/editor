bf8a1e5f7ec8c5ca181c3cbaff08525a4a104b41
Sam Hughes <sam@samuelhughes.com>
2023-08-27 10:13:52 -0700

    Use cmake for building

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-g++ -std=c++20 -Wall -Wextra -g -pthread *.cpp -o qwi
+# g++ -std=c++20 -Wall -Wextra -g -pthread *.cpp -o qwi
+
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Werror -Wall -Wextra" ..
+make -j8
